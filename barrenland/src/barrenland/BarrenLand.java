fe8a8a63dcfbf8d9c09ded3c21af60579bf26db7
Andi <Andi@ANDILI>
2020-04-08 11:17:16 -0500

    updated to handle input from stdin

--- barrenland/src/barrenland/BarrenLand.java
+++ barrenland/src/barrenland/BarrenLand.java
@@ -1,7 +1,9 @@
 package barrenland;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-
 
 /**
  * @author Andi
@@ -12,112 +14,180 @@ public class BarrenLand {
 	private int[][] land;
 	private static final int ROW = 600;
 	private static final int COLUMN = 400;
+	private static final int BARREN_LAND = 1;
+	private static final int FERTILE_LAND = 0;
+	private static boolean validInput;
 
-    public BarrenLand() {    	
-        land = new int[ROW][COLUMN];
-    }
-    /**
-     * This function takes in the coordinates of the barren land and finds the amount of fertile land
-     * @param coordinates
-     * @return Returns a the area of the fertile land in order of smallest to greatest
-     */
-    public List<Integer> getFertileLandArea(String[] coordinates) {
-        List<Integer> result = new ArrayList<>();//create result list
-        if(parseString(coordinates)){ //checks to make sure that the input is valid before performing bfs           
-        	//loops through matrix to check for 0's (fertile land)
-            for (int i = 0; i < land.length; i++) {
-                for (int j = 0; j < land[0].length; j++) {
-                    if (land[i][j] == 0) {
-                        int area = bfs(land, i, j);
-                        result.add(area);//add the area to list
-                    }
-                }
-            }
-            Collections.sort(result);//sort the list from least to greatest
-           
-    
-        }
-        return result;
-       
-    }
-   
-    /**
-     * This function marks the land as barren in this case it marks it as 1(visited)
-     * @param x1
-     * @param y1
-     * @param x2
-     * @param y2
-     */
-    private void fillLand(int x1, int y1, int x2, int y2) {
-        for (int i = y1; i <= y2; i++) {
-            for (int j = x1; j <= x2; j++) {
-                land[i][j] = 1;//mark the tile as visited(barren)
-            }
-        }
-    }
-    /**
-     * This function takes the input string and parses it to extract the coordinates 
-     * @param coordinates
-     * @return
-     */
-    private boolean parseString(String[] coordinates) {
-        for(int i = 0;i<coordinates.length;i++){
-            coordinates[i] = coordinates[i].replaceAll("[^.0-9\\s]", "");//remove non numbers
-        }
-        for (int i = 0; i < coordinates.length; i++) {
-        	 String[] points = coordinates[i].split(" "); 
-             if(points.length>4 || points.length < 4){//checks if the format of the string is correct
-                 return false;
-             }                     
-             int x1 = Integer.valueOf(points[0]);//changes string to int
-             int y1 = Integer.valueOf(points[1]);
-             int x2 = Integer.valueOf(points[2]);
-             int y2 = Integer.valueOf(points[3]);
-           
-             //checks if the numbers are with the 400x600
-             try {
-                 fillLand(x1, y1, x2, y2);
-             } catch (IndexOutOfBoundsException e) {               
-                 System.out.println("Parsed string contains values outside of the 400x600 land");
-                 return false;
-             }         
-           
-        }
+	public BarrenLand() {
+		land = new int[ROW][COLUMN];
+		validInput = true;
+	}
+	/**
+	 * Read the string from stdin and populate the barren land
+	 * @throws IOException
+	 */
+	public void readInput() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			sb.append(br.readLine());
+			if (sb.toString().matches("[0-9\",}{“” ]+")) {
+				break;
+			} else {				
+				System.out.println("Not valid input");
+				break;
+			}
+		}
 
-        return true;
-    }
-    /**
-     * This function performs breadth first search on the adj matrix to count area of the fertile(unvisited land)
-     * @param land
-     * @param x
-     * @param y
-     * @return
-     */
-    private int bfs(int[][] land, int x, int y) {
-        int area = 1;//start the area count
-        
-        int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } }; //check neighbors
-        Queue<int[]> queue = new LinkedList<>(); 
-        queue.offer(new int[] { x, y });
-        land[x][y] = 1; //mark first tile as visited
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            for (int[] dir : dirs) {//loop through adjacent neighbors
-                int newX = curr[0] + dir[0];
-                int newY = curr[1] + dir[1];
-                if (newX < 0 || newX >= land.length || newY < 0 || newY >= land[0].length) {//checks if the neighbor is in bounds
-                    continue;
-                }
-                if (land[newX][newY] == 0) {//fertile land found
-                    area++; //increment area
-                    land[newX][newY] = 1;//change the land to barren
-                    queue.offer(new int[] { newX, newY });//add neighbor to queue
-                }
-            }
-        }
-        return area;
-    }
+		String[] subStrings = sb.toString().split(",");
 
-    
+		for (String s : subStrings) {
+			s = s.replaceAll("[^.0-9\\s]", ""); // remove all non numbers
+			s = s.replaceAll("^ ", "");     // remove leading space
+			parseString(s);
+		}
+
+	}
+
+	/**
+	 * This function takes in the coordinates of the barren land and finds the
+	 * amount of fertile land
+	 * 
+	 * @param listOfCoordinates
+	 * @return Returns a the area of the fertile land in order of smallest to
+	 *         greatest
+	 */
+	public List<Integer> getFertileLandArea() {
+		List<Integer> result = new ArrayList<>();
+		if(validInput) {
+			// loops through matrix to check for 0's (fertile land)
+			for (int i = 0; i < land.length; i++) {
+				for (int j = 0; j < land[0].length; j++) {
+					if (land[i][j] == FERTILE_LAND) {
+						int area = bfs(land, i, j);
+						result.add(area);// add the area to list
+					}
+				}
+			}
+			Collections.sort(result);// sort the list from least to greatest
+		}		
+
+		// return empty list if input not valid
+		System.out.println(result); // print result to std out
+		return result;
+
+	}
+
+	/**
+	 * This function marks the land as barren in this case it marks it as 1(visited)
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 */
+	private void fillLand(int x1, int y1, int x2, int y2) {
+		for (int i = y1; i <= y2; i++) {
+			for (int j = x1; j <= x2; j++) {
+				land[i][j] = BARREN_LAND;// mark the tile as visited(barren)
+			}
+		}
+	}
+
+	/**
+	 * This function takes the input string and parses it to extract the coordinates
+	 * 
+	 * @param listOfCoordinates
+	 * @return
+	 */
+	private void parseString(String listOfCoordinates) {
+		String[] points = listOfCoordinates.split(" ");
+		if (points.length > 4 || points.length < 4) {// checks if the format of the string is correct
+			validInput = false;
+			System.out.println("Invalid Input");
+		}
+		int x1 = Integer.valueOf(points[0]);// changes string to int
+		int y1 = Integer.valueOf(points[1]);
+		int x2 = Integer.valueOf(points[2]);
+		int y2 = Integer.valueOf(points[3]);
+		if (checkBoundries(x1, y1, x2, y2)) {
+			fillLand(x1, y1, x2, y2);
+
+		} else {
+			validInput = false;
+			System.out.println("Points out of bounds");
+
+		}
+	}
+
+	/**
+	 * This functions makes sure that the coordinates are within bounds
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @return
+	 */
+	private boolean checkBoundries(int x1, int y1, int x2, int y2) {
+		if (y1 < 0 || y1 > ROW - 1) {
+			return false;
+		}
+		if (x1 < 0 || x1 > COLUMN - 1) {
+			return false;
+		}
+		if (y2 < 0 || y2 > ROW - 1) {
+			return false;
+		}
+		if (x2 < 0 || x2 > COLUMN - 1) {
+			return false;
+		}
+
+		return true;
+
+	}
+
+	/**
+	 * This function performs breadth first search on the adj matrix to count area
+	 * of the fertile(unvisited land)
+	 * 
+	 * @param land
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	private int bfs(int[][] land, int x, int y) {
+		int area = 1;// start the area count
+
+		int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } }; // check neighbors
+		Queue<int[]> queue = new LinkedList<>();
+		queue.offer(new int[] { x, y });
+		land[x][y] = BARREN_LAND; // mark first tile as visited
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
+			for (int[] dir : dirs) {// loop through adjacent neighbors
+				int newX = curr[0] + dir[0];
+				int newY = curr[1] + dir[1];
+				if (newX < 0 || newX >= land.length || newY < 0 || newY >= land[0].length) {// checks if the neighbor is
+																							// in bounds
+					continue;
+				}
+				if (land[newX][newY] == FERTILE_LAND) {// fertile land found
+					area++; // increment area
+					land[newX][newY] = BARREN_LAND;// change the land to barren
+					queue.offer(new int[] { newX, newY });// add neighbor to queue
+				}
+			}
+		}
+		return area;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public int[][] getLand() {
+		return land;
+	}
 
 }
--- barrenland/src/barrenland/BarrenLandTests.java
+++ barrenland/src/barrenland/BarrenLandTests.java
@@ -2,72 +2,152 @@ package barrenland;
 
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BarrenLandTests {
-	/**
-	 * Test input 1 from example
-	 */
-	@Test
-	public void testGetFertileLandArea1() {
-		BarrenLand bl = new BarrenLand();
-		List<Integer> expectedResult1 = Arrays.asList(116800, 116800);
-		String[] input1 = { "0 292 399 307" };
-		assertEquals(expectedResult1, bl.getFertileLandArea(input1));
+	private BarrenLand bl;
+
+	@BeforeEach
+	public void init() {
+		bl = new BarrenLand();
 
 	}
+
+	/**
+	 * Test input 1 from example
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void testGetFertileLandArea1() throws IOException {
+		List<Integer> expectedResult1 = Arrays.asList(116800, 116800);
+		String input = "0 292 399 307";
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+		bl.readInput();
+		assertEquals(expectedResult1, bl.getFertileLandArea());
+
+	}
+
 	/**
 	 * Test input 2 from example
+	 * 
+	 * @throws IOException
 	 */
+
 	@Test
-	public void testGetFertileLandArea2() {
-		BarrenLand bl = new BarrenLand();
+	public void testGetFertileLandArea2() throws IOException {
+
 		List<Integer> expectedResult2 = Arrays.asList(22816, 192608);
-		String[] input2 = { "48 192 351 207", "48 392 351 407", "120 52 135 547", "260 52 275 547" };
-		assertEquals(expectedResult2, bl.getFertileLandArea(input2));
+		String input = "{“48 192 351 207”, “48 392 351 407”, “120 52 135 547”, “260 52 275 547”}";
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+		bl.readInput();
+		assertEquals(expectedResult2, bl.getFertileLandArea());
 	}
-	
-	/**
-	 * Test what happens if the coordinate is out of bounds
-	 */
-	@Test
-	public void testGetFertileLandArea3() {
-		BarrenLand bl = new BarrenLand();	
-		List<Integer> expectedResult3 = Arrays.asList();
-		String[] input3 = {"0 292 400 307"};
-		assertEquals(expectedResult3, bl.getFertileLandArea(input3));
-	}
+
 	/**
 	 * Test what happens if the land is all barren
+	 * @throws IOException 
 	 */
+
 	@Test
-	public void testGetFertileLandArea4() {
-		BarrenLand bl = new BarrenLand();	
+	public void testGetFertileLandArea4() throws IOException {
+
 		List<Integer> expectedResult4 = Arrays.asList();
-		String[] input4 = {"0 0 399 599"};
-		assertEquals(expectedResult4, bl.getFertileLandArea(input4));
+		String input = "0 0 399 599";
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+		bl.readInput();
+		assertEquals(expectedResult4, bl.getFertileLandArea());
 	}
+
 	/**
 	 * Test what happens if there are non numerical numbers in input
+	 * @throws IOException 
 	 */
+
 	@Test
-	public void testGetFertileLandArea5() {
-		BarrenLand bl = new BarrenLand();	
+	public void testGetFertileLandArea5() throws IOException {
+
 		List<Integer> expectedResult5 = Arrays.asList();
-		String[] input5 = {"a0 0 3d99 599"};
-		assertEquals(expectedResult5, bl.getFertileLandArea(input5));
-	}	
+		String input =  "a0 0 3d99 599";
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+		bl.readInput();
+		assertEquals(expectedResult5, bl.getFertileLandArea());
+	}
+
 	/**
 	 * Test if input has too many numbers
+	 * @throws IOException 
+	 */
+
+	@Test
+	public void testGetFertileLandArea6() throws IOException {
+
+		List<Integer> expectedResult6 = Arrays.asList();
+		String input =  "0 292 399 307 234" ;
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+		bl.readInput();
+		assertEquals(expectedResult6, bl.getFertileLandArea());
+	}
+
+	/**
+	 * Test what happens if the x1 coordinate is out of bounds
+	 * @throws IOException 
+	 */
+
+	@Test
+	public void testGetFertileLandArea7() throws IOException {
+
+		List<Integer> expectedResult = Arrays.asList();
+		String input = "600 292 399 307";
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+		bl.readInput();
+		assertEquals(expectedResult, bl.getFertileLandArea());
+	}
+
+	/**
+	 * Test what happens if the y1 coordinate is out of bounds
+	 * @throws IOException 
+	 */
+
+	@Test
+	public void testGetFertileLandArea8() throws IOException {
+		List<Integer> expectedResult = Arrays.asList();
+		String input = "0 700 399 307" ;
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+		bl.readInput();
+		assertEquals(expectedResult, bl.getFertileLandArea());
+	}
+
+	/**
+	 * Test what happens if the x2 coordinate is out of bounds
+	 * @throws IOException 
+	 */
+
+	@Test
+	public void testGetFertileLandArea9() throws IOException {
+
+		List<Integer> expectedResult = Arrays.asList();
+		String input = "0 292 400 307";
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+		bl.readInput();
+		assertEquals(expectedResult, bl.getFertileLandArea());
+	}
+
+	/**
+	 * Test what happens if the y2 coordinate is out of bounds
+	 * @throws IOException 
 	 */
 	@Test
-	public void testGetFertileLandArea6() {
-		BarrenLand bl = new BarrenLand();	
-		List<Integer> expectedResult6 = Arrays.asList();
-		String[] input6 = {"0 292 399 307 234"};
-		assertEquals(expectedResult6, bl.getFertileLandArea(input6));
+	public void testGetFertileLandArea10() throws IOException {
+
+		List<Integer> expectedResult = Arrays.asList();
+		String input =  "0 292 399 600" ;
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+		bl.readInput();
+		assertEquals(expectedResult, bl.getFertileLandArea());
 	}
-	
 
 }
